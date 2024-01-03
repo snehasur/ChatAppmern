c8e9a8fed8400b7924a618c7518f25a60586cfd5
Sneha Sur <snehasurece@gmail.com>
2024-01-03 15:09:13 +0530

    node changes

--- server/index.js
+++ server/index.js
@@ -9,6 +9,12 @@ require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, PATH, PATCH");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 mongoose
   .connect(process.env.MONGO_URL, {
